297c0bbb060cc3be3fbf536d5f790b1681fec09f
William Shaw <williamkshaw@Mac-mini.local>
2026-06-12 10:13:06 -0400

    UI Tweak: Remove spin animation when switching tabs

--- src/App.tsx
+++ src/App.tsx
@@ -2824,7 +2824,7 @@ const [sharedTab, setSharedTab] = useState<"owned" | "wishlist">("owned");
                   </motion.div>
                 ) : (
                   <motion.div key="bag" initial={{opacity: 0, x: -20}} animate={{opacity: 1, x: 0}} exit={{opacity: 0, x: 20}} transition={{ duration: 0.2 }} className="space-y-4">
-                    <AnimatePresence mode="wait">
+                    <AnimatePresence mode="wait" initial={false}>
                     {showTrophyCase ? (
                       <motion.div key="trophycase" initial={{ rotateY: 90, opacity: 0 }} animate={{ rotateY: 0, opacity: 1 }} exit={{ rotateY: -90, opacity: 0 }} transition={{ duration: 0.3 }} style={{ perspective: 1000 }}>
                         <TrophyCase 
